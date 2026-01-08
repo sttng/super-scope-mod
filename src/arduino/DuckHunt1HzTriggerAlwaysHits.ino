dcc34984e923944515ddd9b59aef7a9461ffc807
sttng <46765778+sttng@users.noreply.github.com>
2026-01-09 00:08:57 +0800

    Update DuckHunt1HzTriggerAlwaysHits.ino

--- src/arduino/DuckHunt1HzTriggerAlwaysHits.ino
+++ src/arduino/DuckHunt1HzTriggerAlwaysHits.ino
@@ -1,29 +1,29 @@
-#define COLOR_PIN D2
-#define TRIGGER_PIN D0
+#define COLOR_PIN 4
+#define TRIGGER_PIN 6
 
 const int singleFrame = 16667;
 const int highTime = 2667;
 const int lowTime = 14000;
 
 void setup() {
-  pinMode(COLOR_PIN, OUTPUT); digitalwrite(COLOR_PIN, LOW);
-  pinMode(TRIGGER_PIN,OUTPUT); digitalwrite(TRIGGER_PIN, LOW);
+  pinMode(COLOR_PIN, OUTPUT); digitalWrite(COLOR_PIN, LOW);
+  pinMode(TRIGGER_PIN,OUTPUT); digitalWrite(TRIGGER_PIN, LOW);
 }
 
 void loop(){
-  digitalwrite(TRIGGER_PIN, HIGH);
-  for (int i=0; i<3; i++){
-    digitalwrite(COLOR_PIN,HIGH); delayMicroseconds(highTime);
-    digitalwrite(COLOR_PIN, LOW); delayMicroseconds (lowTime);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  for (int i = 0; i < 3; i++){
+    digitalWrite(COLOR_PIN,HIGH); delayMicroseconds(highTime);
+    digitalWrite(COLOR_PIN, LOW); delayMicroseconds (lowTime);
   }
   delayMicroseconds (singleFrame);
-  for (int 1=0; i‹50; i++){
-    digitalwrite(COLOR_PIN,HIGH); delayMicroseconds(highTime);
+  for (int i = 0; i < 50; i++){
+    digitalWrite(COLOR_PIN,HIGH); delayMicroseconds(highTime);
     digitalWrite(COLOR_PIN,LOW); delayMicroseconds(lowTime);
   }
-  digitalwrite(TRIGGER_PIN, LOW)
-  for (int i=0; i‹6; i++){
+  digitalWrite(TRIGGER_PIN, LOW);
+  for (int i = 0; i < 6; i++){
     digitalWrite(COLOR_PIN,HIGH); delayMicroseconds(highTime);
-    digitalwrite(COLOR_PIN,LOW); delayMicroseconds (lowTime);
+    digitalWrite(COLOR_PIN,LOW); delayMicroseconds (lowTime);
   }
 }
