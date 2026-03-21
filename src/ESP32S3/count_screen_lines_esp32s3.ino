2f702c8d383561f312f581e120926cc93eaabdd0
sttng <46765778+sttng@users.noreply.github.com>
2026-03-21 11:53:18 +0100

    Add files via upload

---
+++ src/ESP32S3/count_screen_lines_esp32s3.ino
@@ -0,0 +1,45 @@
+/*
+  For ESP32S3 Dev Module
+
+For serial output to work:
+  Select Tools > USB CDC On Boot > Enabled from the Arduino IDE menus.
+*/
+
+const int COMPOSITE_SYNC_PIN = 6;
+const int VERTICAL_SYNC_PIN = 7;
+
+volatile int lineCount = 0;
+volatile int previousLineCount = 0;
+
+volatile unsigned long lineStartTime = 0;
+volatile unsigned long previousLineStartTime = 0;
+volatile unsigned long elapsedLineTime = 0;
+
+volatile int frameCounter = 0;
+volatile bool inVsync = false;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(COMPOSITE_SYNC_PIN, INPUT);
+  pinMode(VERTICAL_SYNC_PIN, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(COMPOSITE_SYNC_PIN), handleCompositeSync, RISING);
+  attachInterrupt(digitalPinToInterrupt(VERTICAL_SYNC_PIN), handleVerticalSync, CHANGE);
+}
+
+void loop() {
+  //Serial.println(previousLineCount);
+  if (lineCount == 0 && previousLineCount > 15) {
+    Serial.println(previousLineCount);
+  }
+}
+
+void handleCompositeSync() {
+  lineCount++;
+}
+
+void handleVerticalSync() {
+  previousLineCount = lineCount;
+  lineCount = 0;
+}
---
+++ src/ESP32S3/simulate_receiver_esp32s3.ino
@@ -0,0 +1,93 @@
+/*
+  For ESP32S3 Dev Module
+
+For serial output to work:
+  Select Tools > USB CDC On Boot > Enabled from the Arduino IDE menus.
+*/
+
+const int COMPOSITE_SYNC_PIN = 6;
+const int VERTICAL_SYNC_PIN = 7;
+
+const int TRIGGER_PIN = 5;
+const int CURSOR_PIN = 1;
+const int TURBO_PIN = 3;
+const int PAUSE_PIN = 4;
+const int EXTERNAL_LATCH_PIN = 2; // Raster gate
+
+const int minAimLine = 10;
+const int maxAimLine = 240;
+const int minAimMicroseconds = 5;
+const int maxAimMicroseconds = 55;
+
+volatile int lineCount = 0;
+volatile int aimLine = 120; // 10 - 240
+volatile int aimMicroseconds = 27; // 5 - 55
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(COMPOSITE_SYNC_PIN, INPUT);
+  pinMode(VERTICAL_SYNC_PIN, INPUT);
+
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(CURSOR_PIN, OUTPUT);
+  pinMode(TURBO_PIN, OUTPUT);
+  pinMode(PAUSE_PIN, OUTPUT);
+  pinMode(EXTERNAL_LATCH_PIN, OUTPUT);
+
+  digitalWrite(TRIGGER_PIN, HIGH);
+  digitalWrite(CURSOR_PIN, HIGH);
+  digitalWrite(TURBO_PIN, HIGH);
+  digitalWrite(PAUSE_PIN, HIGH);
+  digitalWrite(EXTERNAL_LATCH_PIN, HIGH);
+
+  attachInterrupt(digitalPinToInterrupt(COMPOSITE_SYNC_PIN), handleCompositeSync, RISING);
+  attachInterrupt(digitalPinToInterrupt(VERTICAL_SYNC_PIN), handleVerticalSync, RISING) ;
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    int inputByte = Serial.read();
+
+    switch (inputByte) {
+      case 'w': case 'W':
+        aimLine -= 40;
+        if (aimLine < minAimLine) aimLine = minAimLine;
+        break;
+      case 'a': case 'A':
+        aimMicroseconds -= 10;
+        if (aimMicroseconds < minAimMicroseconds) aimMicroseconds = minAimMicroseconds;
+        break;
+      case 's': case 'S':
+        aimLine += 40;
+        if (aimLine > maxAimLine) aimLine = maxAimLine;
+        break;
+      case 'd': case 'D':
+        aimMicroseconds += 10;
+        if (aimMicroseconds > maxAimMicroseconds) aimMicroseconds = maxAimMicroseconds;
+        break;
+      case 't': case 'T':
+        digitalWrite(TRIGGER_PIN, LOW);
+        delay(250);
+        digitalWrite(TRIGGER_PIN, HIGH);
+
+        Serial.println("Trigger pressed.");
+        break;
+    }
+  }
+}
+
+void handleCompositeSync() {
+  lineCount++;
+
+  if (lineCount == aimLine) { 
+    delayMicroseconds(aimMicroseconds);
+    digitalWrite(EXTERNAL_LATCH_PIN, LOW);
+    delayMicroseconds(5);
+    digitalWrite(EXTERNAL_LATCH_PIN, HIGH);
+  }
+}
+
+void handleVerticalSync() {
+  lineCount = 0;
+}
