783a00346de401fc48a5b375e5423b875ccd3e29
sttng <46765778+sttng@users.noreply.github.com>
2026-03-29 21:01:57 +0800

    Update super_scope_ble.py

--- src/pi/super_scope_ble.py
+++ src/pi/super_scope_ble.py
@@ -3,15 +3,15 @@ import numpy
 import os
 import RPi.GPIO as GPIO
 import time
-import serial
 
 from imutils import perspective
 
-import asyncio
-from bleak import BleakClient
+import simplepyble
 
 BLE_ADDRESS = "D0:CF:13:2F:C3:C5"
+SERVICE_UUID ="4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+DEFAULT_PI_BLE_ADAPTER_IDX = 0
 
 TRACKING_WIDTH = 51
 BLACK_BAR_WIDTH = 8
@@ -46,102 +46,65 @@ GPIO.setup(PAUSE_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(TRIGGER_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(CURSOR_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-async def handleButtons(channel):
+def on_scan_start():
+    print("Scan started.")
+def on_scan_stopped():
+    print("Scan complete.")
+def on_device_found(peripheral):
+    if(peripheral.address() == BLE_ADDRESS):
+        print(f"Found {peripheral.identifier()} [{peripheral.address()}]")
+def on_notification(data):
+    print(f"Notification: {data}")
+
+
+adapters = simplepyble.Adapter.get_adapters()
+if len(adapters) == 0:
+    print("No adapters found")
+adapter = adapters[DEFAULT_PI_BLE_ADAPTER_IDX]
+print(f"Selected Pi adapter: {adapter.identifier()} [{adapter.address()}]")
+adapter.set_callback_on_scan_start(on_scan_start)
+adapter.set_callback_on_scan_stop(on_scan_stopped)
+adapter.set_callback_on_scan_found(on_device_found)
+adapter.scan_for(5000)
+    
+peripherals = adapter.scan_get_results()
+selectedperi = 0
+
+for i, peripheral in enumerate(peripherals):
+    if(peripheral.address() == BLE_ADDRESS):
+        selectedperi = peripheral
+            
+peripheral = selectedperi
+print(f"Connecting to: {peripheral.identifier()} [{peripheral.address()}]")
+peripheral.connect()
+service_uuid = SERVICE_UUID
+characteristic_uuid = CHARACTERISTIC_UUID
+
+
+def handleButtons(channel):
     isRising = GPIO.input(channel)
-    
-    async with BleakClient(BLE_ADDRESS) as client:
-        if channel == TURBO_PIN:
-            await client.write_gatt_char(CHARACTERISTIC_UUID, BT_MESSAGE_TURBO_ENABLED.encode("utf-8") if isRising else BT_MESSAGE_TURBO_DISABLED.encode("utf-8"))
-        elif channel == POWER_ON_PIN:
-            if not isRising:
-                os.system("shutdown now -h")
-        elif channel == PAUSE_BUTTON_PIN:
-            await client.write_gatt_char(CHARACTERISTIC_UUID, BT_MESSAGE_PAUSE_PRESSED.encode("utf-8") if isRising else BT_MESSAGE_PAUSE_RELEASED.encode("utf-8"))
-        elif channel == TRIGGER_BUTTON_PIN:
-            await client.write_gatt_char(CHARACTERISTIC_UUID, BT_MESSAGE_TRIGGER_PRESSED.encode("utf-8") if isRising else BT_MESSAGE_TRIGGER_RELEASED.encode("utf-8"))
-        elif channel == CURSOR_BUTTON_PIN:
-            await client.write_gatt_char(CHARACTERISTIC_UUID, BT_MESSAGE_CURSOR_PRESSED.encode("utf-8") if isRising else BT_MESSAGE_CURSOR_RELEASED.encode("utf-8"))
+
+    if channel == TURBO_PIN:
+        peripheral.write_request(service_uuid, characteristic_uuid, str.encode(BT_MESSAGE_TURBO_ENABLED) if isRising else str.encode(BT_MESSAGE_TURBO_DISABLED))
+    elif channel == POWER_ON_PIN:
+        if not isRising:
+            os.system("shutdown now -h")
+    elif channel == PAUSE_BUTTON_PIN:
+        peripheral.write_request(service_uuid, characteristic_uuid, str.encode(BT_MESSAGE_PAUSE_PRESSED) if isRising else str.encode(BT_MESSAGE_PAUSE_RELEASED))
+    elif channel == TRIGGER_BUTTON_PIN:
+        peripheral.write_request(service_uuid, characteristic_uuid, str.encode(BT_MESSAGE_TRIGGER_PRESSED) if isRising else str.encode(BT_MESSAGE_TRIGGER_RELEASED))
+    elif channel == CURSOR_BUTTON_PIN:
+        peripheral.write_request(service_uuid, characteristic_uuid, str.encode(BT_MESSAGE_CURSOR_PRESSED) if isRising else str.encode(BT_MESSAGE_CURSOR_RELEASED))
+
+handleButtons(TURBO_PIN)
+
+GPIO.add_event_detect(TURBO_PIN, GPIO.BOTH, callback = handleButtons, bouncetime = 10)
+GPIO.add_event_detect(POWER_ON_PIN, GPIO.BOTH, callback = handleButtons, bouncetime = 10)
+GPIO.add_event_detect(PAUSE_BUTTON_PIN, GPIO.BOTH, callback = handleButtons, bouncetime = 10)
+GPIO.add_event_detect(TRIGGER_BUTTON_PIN, GPIO.BOTH, callback = handleButtons, bouncetime = 10)
+GPIO.add_event_detect(CURSOR_BUTTON_PIN, GPIO.BOTH, callback = handleButtons, bouncetime = 10)
 
 
-async def main():
-    handleButtons(TURBO_PIN)
-
-    GPIO.add_event_detect(TURBO_PIN, GPIO.BOTH, callback = handleButtons, bouncetime = 10)
-    GPIO.add_event_detect(POWER_ON_PIN, GPIO.BOTH, callback = handleButtons, bouncetime = 10)
-    GPIO.add_event_detect(PAUSE_BUTTON_PIN, GPIO.BOTH, callback = handleButtons, bouncetime = 10)
-    GPIO.add_event_detect(TRIGGER_BUTTON_PIN, GPIO.BOTH, callback = handleButtons, bouncetime = 10)
-    GPIO.add_event_detect(CURSOR_BUTTON_PIN, GPIO.BOTH, callback = handleButtons, bouncetime = 10)
-    
-    camera = cv2.VideoCapture(0)
-
-    if camera.isOpened():
-        camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    
-    windowName = "Super Scope"
-    cv2.namedWindow(windowName)
-    
-    while True:
-        wasFrameGrabSuccessful, frame = camera.read()
-    
-        if wasFrameGrabSuccessful:
-            grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            _, threshold = cv2.threshold(grayFrame, 127, 255, cv2.THRESH_BINARY)
-            contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-            finalFrame = cv2.cvtColor(grayFrame, cv2.COLOR_GRAY2BGR)
-    
-            trackingPointCount = 0
-            trackingPoints = numpy.zeros(shape = (4, 2), dtype = numpy.float32)
-    
-            for contour in contours:
-                moments = cv2.moments(contour)
-    
-                if moments["m00"] > 10:
-                    if trackingPointCount < 5:
-                        centerX = int(moments["m10"] / moments["m00"])
-                        centerY = int(moments["m01"] / moments["m00"])
-                        trackingPoints[trackingPointCount] = [centerX, centerY]
-                        cv2.circle(finalFrame, (centerX, centerY), 5, (0, 255, 0), cv2.FILLED)
-    
-                    trackingPointCount += 1
-    
-            trackingPoints = perspective.order_points(trackingPoints)
-    
-            # Draw red dot at center.
-            cv2.circle(finalFrame, (320, 240), 5, (0, 0, 255), cv2.FILLED)
-    
-            # Outline transformed area in blue.
-            cv2.rectangle(finalFrame, (0, 0), (TRACKING_WIDTH, TRACKING_HEIGHT), (255, 0, 0), 3)
-    
-            if trackingPointCount == 4:
-                destPoints = numpy.array([[0, 0], [WIDE_TRACKING_WIDTH - 1, 0], [WIDE_TRACKING_WIDTH - 1, TRACKING_HEIGHT - 1], [0, TRACKING_HEIGHT - 1]], dtype = numpy.float32)
-                transform = cv2.getPerspectiveTransform(trackingPoints[:, numpy.newaxis, :], destPoints[:, numpy.newaxis, :])
-                transformed = cv2.perspectiveTransform(numpy.array([[320, 240]], dtype = numpy.float32)[:, numpy.newaxis, :], transform)
-    
-                transformedX = int(transformed[0][0][0] - BLACK_BAR_WIDTH)
-                transformedY = int(transformed[0][0][1])
-    
-                # Draw transformed tracking point in yellow.
-                cv2.circle(finalFrame, (transformedX, transformedY), 5, (0, 255, 255), cv2.FILLED)
-    
-                if (0 <= transformedX < TRACKING_WIDTH) and (0 <= transformedY < TRACKING_HEIGHT):
-                    isTvVisible = True;
-                    serialPort.write(bytearray([BT_MESSAGE_AIM_X, transformedX, BT_MESSAGE_AIM_Y, transformedY]))
-                elif isTvVisible:
-                    isTvVisible = False;
-                    serialPort.write(bytearray([BT_MESSAGE_TV_NOT_VISIBLE]))
-            elif isTvVisible:
-                isTvVisible = False;
-                serialPort.write(bytearray([BT_MESSAGE_TV_NOT_VISIBLE]))
-    
-            cv2.imshow(windowName, finalFrame)
-    
-        if cv2.waitKey(1) % 0xFF == 27:  # Escape key
-            break
-    
-    camera.release()
-    cv2.destroyAllWindows()
-    GPIO.cleanup()
-
-asyncio.run(main())
+while True:
+    print ("-----")
+    time.sleep(0.5)
